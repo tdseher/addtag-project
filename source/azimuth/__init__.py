499a0f7c11a13973bbe034563d478159a03af54f
Thaddeus D. Seher <tseher@ucmerced.edu>
2017-04-14 09:11:41 -0700

    Completed 'source/azimuth/wrapper.py' so it now calculates the Azimuth score using the default parameters. Completed 'source/azimuth/__init__.py' so it calls the wrapper script and returns a single Azimuth score. Contains a little bit of redundant code for anchoring the SPACER>PAM within the up/down-stream sequences.

--- source/azimuth/__init__.py
+++ source/azimuth/__init__.py
@@ -9,25 +9,71 @@ import sys
 import os
 import subprocess
 
+import regex
+
 # Azimuth requires Python 2.7
 # as well as specific versions of pandas, scipy, scikit-learn, etc
 # So rather than call it natively, we will open it using subprocess
 
 def azimuth_score(seq, pam, upstream='', downstream=''):
     #error_file = os.path.join(folder, name + '.err')
-    error_file = os.path.join('azimuth.err')
+    #error_file = os.path.join('azimuth.err')
     
-    command_list = ['python', 'azimuth.py', seq]
+    # ACAG CTGATCTCCAGATATGACCA|TGG GTT
+    # CAGC TGATCTCCAGATATGACCAT|GGG TTT
+    # CCAG AAGTTTGAGCCACAAACCCA|TGG TCA
     
-    with open(error_file, 'w+') as flo:
-        cp = subprocess.run(command_list, shell=False, check=True, stdout=flo, stderr=subprocess.STDOUT)
-    
+    new = ['-'] * 30
+    for i, nt in enumerate(pam):
+        new[24+i] = nt
 
+    for i, nt in enumerate(seq[::-1]):
+        new[24-1-i] = nt
+
+    for i, nt in enumerate(upstream[::-1]):
+        ind = 24-len(seq)-1-i
+        if (ind < 0):
+            break
+        new[ind] = nt
+
+    for i, nt in enumerate(downstream):
+        ind = 24+len(pam)+i
+        if (ind >= len(new)):
+            break
+        new[ind] = nt
+    query = ''.join(new)
+    
+    if (len(query) != 30):
+        return 0.0
+    
+    m = regex.search('[^ATCGatcg]', query)
+    if m:
+        return 0.0
+    
+    # Obtain path of currently-running file
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+    WRAPPER_PATH = os.path.join(SCRIPT_DIR, "wrapper.py")
+    # Should use args.python2_path
+    command_list = ['python', WRAPPER_PATH, query]
+    
+    #with open(error_file, 'w+') as flo:
+    #    cp = subprocess.run(command_list, shell=False, check=True, stdout=flo, stderr=subprocess.STDOUT)
+    cp = subprocess.run(command_list, shell=False, check=True, stdout=subprocess.PIPE)
+    output = cp.stdout.decode()
+    
+    o_seq = None
+    o_score = 0.0
+    for line in output.splitlines():
+        if not line.startswith('No model file specified'):
+            o_seq, o_score = line.split(" ")
+    
+    return float(o_score)
 
 def test():
     """Code to test the classes and functions in 'source/azimuth/__init__.py'"""
     
-    pass
+    us, seq, pam, ds = 'ACAG', 'CTGATCTCCAGATATGACCA', 'TGG', 'GTT'
+    print(us+' '+seq+'|'+pam+' '+ds, azimuth_score(seq, pam, upstream=us, downstream=ds))
 
 if (__name__ == "__main__"):
     test()
--- source/azimuth/wrapper.py
+++ source/azimuth/wrapper.py
@@ -10,11 +10,10 @@ import azimuth.model_comparison
 import numpy as np
 
 # Raise a useful error if utilizing an incompatible version of Python
-if ((sys.version_info < (2, 7, 12)) or (sys.version_info >= (3, 0, 0))):
-    raise Exception("AddTag Azimuth wrapper requires 3.0.0 > Python >= 2.7.12. You are running with " + ".".join(map(str, sys.version_info[:3])) + ".")
+if ((sys.version_info < (2, 7, 10)) or (sys.version_info >= (3, 0, 0))):
+    raise Exception("AddTag Azimuth wrapper requires 3.0.0 > Python >= 2.7.10. You are running with " + ".".join(map(str, sys.version_info[:3])) + ".")
 
-def main():
-    
+def test():
     sequences = np.array(['ACAGCTGATCTCCAGATATGACCATGGGTT', 'CAGCTGATCTCCAGATATGACCATGGGTTT', 'CCAGAAGTTTGAGCCACAAACCCATGGTCA'])
     amino_acid_cut_positions = np.array([2, 2, 4])
     percent_peptides = np.array([0.18, 0.18, 0.35])
@@ -27,6 +26,33 @@ def main():
     azimuth.model_comparison.predict(np.array(['GGGAGGCTGCTTTACCCGCTGTGGGGGCGC']), np.array([-1]), np.array([-1]))
     # No model file specified, using V3_model_nopos
     # array([ 0.58477438])
+
+def main():
+    # All nucleotides must have length of 30
+    for arg in sys.argv[1:]:
+        if (len(arg) != 30):
+            exit(1)
+    
+    sequences = np.array(sys.argv[1:])
+    #amino_acid_cut_positions = np.array([-1]*len(sys.argv[1:]))
+    #percent_peptides = np.array([-1]*len(sys.argv[1:]))
+    # Help on function predict in module azimuth.model_comparison:
+    # predict(seq, aa_cut=None, percent_peptide=None, model=None, model_file=None, pam_audit=True, length_audit=False, learn_options_override=None)
+    # Args:
+    #     seq: numpy array of 30 nt sequences.
+    #     aa_cut: numpy array of amino acid cut positions (optional).
+    #     percent_peptide: numpy array of percent peptide (optional).
+    #     model: model instance to use for prediction (optional).
+    #     model_file: file name of pickled model to use for prediction (optional).
+    #     pam_audit: check PAM of each sequence.
+    #     length_audit: check length of each sequence.
+    #     learn_options_override: a dictionary indicating which learn_options to override (optional).
+    # Returns: a numpy array of predictions.
+    predictions = azimuth.model_comparison.predict(sequences, pam_audit=False)
+    #predictions = azimuth.model_comparison.predict(sequences, amino_acid_cut_positions, percent_peptides)
+    
+    for i, prediction in enumerate(predictions):
+        print sequences[i], prediction
 
 if (__name__ == '__main__'):
     main()
