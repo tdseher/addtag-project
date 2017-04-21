b70d2361a6b35deac78e1fe2d84a755863599b3e
Thaddeus D. Seher <tseher@ucmerced.edu>
2017-04-20 21:06:59 -0700

    Shifted Azimuth score from 0-1 to 0-100. Added batch calculation to minimize calls to 'wrapper.py'.

--- source/azimuth/__init__.py
+++ source/azimuth/__init__.py
@@ -15,10 +15,36 @@ import regex
 # as well as specific versions of pandas, scipy, scikit-learn, etc
 # So rather than call it natively, we will open it using subprocess
 
-def azimuth_score(seq, pam, upstream='', downstream=''):
-    #error_file = os.path.join(folder, name + '.err')
-    #error_file = os.path.join('azimuth.err')
+def batch_azimuth_score(sequence_list):
+    queries = []
     
+    for query in sequence_list:
+        seq, pam, upstream, downstream = query
+        queries.append(build_query(seq, pam, upstream, downstream))
+    
+    # Obtain path of currently-running file
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+    WRAPPER_PATH = os.path.join(SCRIPT_DIR, "wrapper.py")
+    # Should use args.python2_path
+    command_list = ['python', WRAPPER_PATH] + queries
+    
+    #with open(error_file, 'w+') as flo:
+    #    cp = subprocess.run(command_list, shell=False, check=True, stdout=flo, stderr=subprocess.STDOUT)
+    cp = subprocess.run(command_list, shell=False, check=True, stdout=subprocess.PIPE)
+    output = cp.stdout.decode()
+    
+    o_scores = []
+    for line in output.splitlines():
+        if not line.startswith('No model file specified'):
+            i_seq, i_score = line.split(" ")
+            o_scores.append(100*float(i_score))
+    
+    return o_scores
+    
+    
+    
+def build_query(seq, pam, upstream='', downstream=''):
+    # us     seq                pam  ds
     # ACAG CTGATCTCCAGATATGACCA|TGG GTT
     # CAGC TGATCTCCAGATATGACCAT|GGG TTT
     # CCAG AAGTTTGAGCCACAAACCCA|TGG TCA
@@ -42,6 +68,13 @@ def azimuth_score(seq, pam, upstream='', downstream=''):
             break
         new[ind] = nt
     query = ''.join(new)
+    return query
+
+def azimuth_score(seq, pam, upstream='', downstream=''):
+    #error_file = os.path.join(folder, name + '.err')
+    #error_file = os.path.join('azimuth.err')
+    
+    query = build_query(seq, pam, upstream, downstream)
     
     if (len(query) != 30):
         return 0.0
@@ -67,7 +100,7 @@ def azimuth_score(seq, pam, upstream='', downstream=''):
         if not line.startswith('No model file specified'):
             o_seq, o_score = line.split(" ")
     
-    return float(o_score)
+    return 100*float(o_score)
 
 def test():
     """Code to test the classes and functions in 'source/azimuth/__init__.py'"""
