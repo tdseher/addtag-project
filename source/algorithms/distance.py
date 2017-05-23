dde529fbc3bd7afa8db40956b2e9cd5c14500e52
Thaddeus D. Seher <tseher@ucmerced.edu>
2017-05-23 16:22:34 -0700

    Added template code for 'Substitutions', 'Insertions', 'Deletions', and 'Errors' classes that returns scores of '0.0', as well as a 'test()' function.

--- source/algorithms/distance.py
+++ source/algorithms/distance.py
@@ -26,13 +26,119 @@ else:
     from ..nucleotides import rc
 
 class Substitutions(PairedSequenceAlgorithm):
-    pass
+    def __init__(self):
+        super().__init__("Substitutions", "Seher", 2017,
+            citation="AddTag",
+            off_target=False,
+            prefilter=False,
+            postfilter=False,
+            minimum=0.0,
+            maximum=5.0,
+            default=0.0
+        )
+    
+    def calculate(self, intended, potential, *args, **kwargs):
+        on_sequence, on_target, on_pam, on_upstream, on_downstream = intended
+        off_sequence, off_target, off_pam, off_upstream, off_downstream = potential
+        
+        return self.score(on_target, off_target)
+    
+    def score(self, seq1, seq2):
+        """"""
+        return 0.0
 
 class Insertions(PairedSequenceAlgorithm):
-    pass
+    def __init__(self):
+        super().__init__("Insertions", "Seher", 2017,
+            citation="AddTag",
+            off_target=False,
+            prefilter=False,
+            postfilter=False,
+            minimum=0.0,
+            maximum=2.0,
+            default=0.0
+        )
+    
+    def calculate(self, intended, potential, *args, **kwargs):
+        on_sequence, on_target, on_pam, on_upstream, on_downstream = intended
+        off_sequence, off_target, off_pam, off_upstream, off_downstream = potential
+        
+        return self.score(on_target, off_target)
+    
+    def score(self, seq1, seq2):
+        """"""
+        return 0.0
 
 class Deletions(PairedSequenceAlgorithm):
-    pass
+    def __init__(self):
+        super().__init__("Deletions", "Seher", 2017,
+            citation="AddTag",
+            off_target=False,
+            prefilter=False,
+            postfilter=False,
+            minimum=0.0,
+            maximum=2.0,
+            default=0.0
+        )
+    
+    def calculate(self, intended, potential, *args, **kwargs):
+        on_sequence, on_target, on_pam, on_upstream, on_downstream = intended
+        off_sequence, off_target, off_pam, off_upstream, off_downstream = potential
+        
+        return self.score(on_target, off_target)
+    
+    def score(self, seq1, seq2):
+        """"""
+        return 0.0
 
 class Errors(PairedSequenceAlgorithm):
-    pass
+    def __init__(self):
+        super().__init__("Errors", "Seher", 2017,
+            citation="AddTag",
+            off_target=False,
+            prefilter=False,
+            postfilter=False,
+            minimum=0.0,
+            maximum=5.0,
+            default=0.0
+        )
+    
+    def calculate(self, intended, potential, *args, **kwargs):
+        on_sequence, on_target, on_pam, on_upstream, on_downstream = intended
+        off_sequence, off_target, off_pam, off_upstream, off_downstream = potential
+        
+        return self.score(on_target, off_target)
+    
+    def score(self, seq1, seq2):
+        """"""
+        return 0.0
+
+def test():
+    """Code to test the functions and classes"""
+    
+    a = ('', 'CGATGGCTAGGATCGATTGA', 'TGG', '', '')
+    b = ('', 'RYMKWSACGTbDHVNACGTA', 'TGG', '', '')
+    c = ('',   'ATGSCTCGGATCGATTGA', 'AGG', '', '')
+    
+    print("=== Substitutions ===")
+    C = Substitutions()
+    print(C.calculate(a, b))
+    print(C.calculate(a, c))
+    
+    print("=== Insertions ===")
+    C = Insertions()
+    print(C.calculate(a, b))
+    print(C.calculate(a, c))
+    
+    print("=== Deletions ===")
+    C = Deletions()
+    print(C.calculate(a, b))
+    print(C.calculate(a, c))
+    
+    print("=== Errors ===")
+    C = Errors()
+    print(C.calculate(a, b))
+    print(C.calculate(a, c))
+
+if (__name__ == "__main__"):
+    test()
