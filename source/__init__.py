a5a26a5eb88a4e97b17c83ecc6769dbd7167446d
Thaddeus D. Seher <tseher@ucmerced.edu>
2018-10-19 11:46:38 -0700

    Added (now unused) functions to pickle objects.

--- source/__init__.py
+++ source/__init__.py
@@ -12,6 +12,7 @@ import argparse
 import time
 import logging
 import random
+import pickle
 
 # Import non-standard packages
 import regex
@@ -3928,3 +3929,17 @@ def target_filter(sequence, target, pam, upstream, downstream, args):
     #    rets.append((temp_seqs2[i], temp_targets2[i], temp_pams2[i]))
     #return rets
     return seqs3
+
+def save_object(obj, name, path='/dev/shm/addtag'):
+    if isinstance(name, str):
+        if (len(name) > 0):
+            os.makedirs(path, exist_ok=True)
+            with open(os.path.join(path, name), 'wb') as flo:
+                pickle.dump(obj, flo, pickle.HIGHEST_PROTOCOL)
+        else:
+            raise Exception("object name is not a string")
+
+def load_object(name, path='/dev/shm/addtag'):
+    with open(os.path.join(path, name), 'rb') as flo:
+        data = pickle.load(flo)
+    return data
